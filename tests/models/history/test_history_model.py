0a7d341266a5a3ec16220a2a323d30333b764ebf
Amanda Zanata <amandazanata46@gmail.com>
2023-09-21 19:36:21 -0300

    Requisito 7

--- tests/models/history/test_history_model.py
+++ tests/models/history/test_history_model.py
@@ -1,7 +1,27 @@
-# import json
-# from src.models.history_model import HistoryModel
+import json
+from src.models.history_model import HistoryModel
 
+expected = [
+    {
+        "text_to_translate": "Hello, I like videogame",
+        "translate_from": "en",
+        "translate_to": "pt",
+    },
+    {
+        "text_to_translate": "Do you love music?",
+        "translate_from": "en",
+        "translate_to": "pt",
+    },
+]
 
 # Req. 7
+
+
 def test_request_history():
-    raise NotImplementedError
+    json_list = HistoryModel.list_as_json()
+    languages_list = json.loads(json_list)
+
+    for translation in languages_list:
+        translation.pop("_id", None)
+
+    assert languages_list == expected
