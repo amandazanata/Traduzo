bc4de5cd57929fd3b7a8ac22f447bf7879b73410
Amanda Zanata <amandazanata46@gmail.com>
2023-09-21 18:17:59 -0300

    Requisitos 1 2 3 4 5 6

--- src/controllers/translate_controller.py
+++ src/controllers/translate_controller.py
@@ -1,7 +1,7 @@
-from flask import Blueprint, render_template
-# from deep_translator import GoogleTranslator
-# from models.language_model import LanguageModel
-# from models.history_model import HistoryModel
+from flask import Blueprint, render_template, request
+from deep_translator import GoogleTranslator
+from models.language_model import LanguageModel
+from models.history_model import HistoryModel
 
 
 translate_controller = Blueprint("translate_controller", __name__)
@@ -10,10 +10,61 @@ translate_controller = Blueprint("translate_controller", __name__)
 # Reqs. 4 e 5
 @translate_controller.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    languages = LanguageModel.list_dicts()
+    to_translate = ""
+    translated_from = "pt"
+    translate_to = "en"
+    translated = "Tradução"
+    translation = ""
+    if request.method == "POST":
+        to_translate = request.form.get("text-to-translate")
+        translate_from = request.form.get("translate-from")
+        translate_to = request.form.get("translate-to")
+
+        translation = GoogleTranslator(
+            source=translate_from, target=translate_to
+        ).translate(
+            to_translate
+        )
+        history = HistoryModel(
+            {
+                "text_to_translate": to_translate,
+                "translate_from": translate_from,
+                "translated_text": translation,
+            }
+        )
+        history.save()
+
+    return render_template(
+        "index.html",
+        text_to_translate=to_translate,
+        translate_from=translated_from,
+        translate_to=translate_to,
+        translated=translation or translated,
+        languages=languages,
+    )
 
 
 # Req. 6
 @translate_controller.route("/reverse", methods=["POST"])
 def reverse():
-    raise NotImplementedError
+    text_to_translate = request.form.get(
+        "text-to-translate", "O que deseja traduzir"
+    )
+    translate_from = request.form.get("translate-from", "pt")
+    translate_to = request.form.get("translate-to", "en")
+
+    translated_text = GoogleTranslator(
+        source=translate_from, target=translate_to
+    ).translate(text_to_translate)
+
+    languages = LanguageModel.list_dicts()
+
+    return render_template(
+        "index.html",
+        languages=languages,
+        text_to_translate=text_to_translate,
+        translate_from=translate_to,
+        translate_to=translate_from,
+        translated=translated_text,
+    )
--- src/models/language_model.py
+++ src/models/language_model.py
@@ -1,17 +1,20 @@
-# from .abstract_model import AbstractModel
-# from database.db import db
+from .abstract_model import AbstractModel
+from database.db import db
 
 
 # Req. 1
-class LanguageModel:
-    def __init__(self):
-        raise NotImplementedError
+class LanguageModel(AbstractModel):
+    _collection = db["languages"]
+
+    def __init__(self, dict_data):
+        super().__init__(dict_data)
+        self._dict_data = self.data
 
     # Req. 2
     def to_dict(self):
-        raise NotImplementedError
+        return {"name": self.data["name"], "acronym": self.data["acronym"]}
 
     # Req. 3
     @classmethod
     def list_dicts(cls):
-        raise NotImplementedError
+        return [lang for lang in cls._collection.find({}, {"_id": 0})]
